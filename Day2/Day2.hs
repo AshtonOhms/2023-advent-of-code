fd0688006d5b8676aea4e0413c8aa8150ab6a220
Ashton Ohms <me@ashtonoh.ms>
2023-12-02 14:30:16 -0800

    Day 2 p1

---
+++ Day2/Day2.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Day2 (main) where
+
+import Data.List (foldl')
+import Data.Text (Text, splitOn, unpack, pack)
+
+type GameId = Int
+data Game = Game GameId [(Int, Int, Int)] -- r, g, b
+            deriving Show
+
+gId :: Game -> GameId
+gId (Game id _) = id
+
+readGame :: Text -> Game
+readGame s = Game id draws
+    where [labelS, drawsS] = splitOn ": " s
+          ["Game", idS] = splitOn " " labelS
+          id = read $ unpack idS
+
+          drawSs = splitOn "; " drawsS
+          draws = map readDraw drawSs
+
+          readDraw :: Text -> (Int, Int, Int)
+          readDraw dS = foldl' (\(r, g, b) cS ->
+                 let [valS, color] = splitOn " " cS
+                     val = read $ unpack valS
+                  in case color of
+                    "red" -> (val, g, b)
+                    "green" -> (r, val, b)
+                    "blue" -> (r, g, val)
+                    _ -> error $ "No color " ++ unpack color
+
+            ) (0, 0, 0) (splitOn ", " dS)
+
+gamePossibleWith :: (Int, Int, Int) -> Game -> Bool
+gamePossibleWith (r, g, b) (Game _ ds) = all drawPossible ds
+    where drawPossible (dr, dg, db) = dr <= r && dg <= g && db <=b
+
+main :: IO ()
+main = do
+    input <- readFile "input.txt"
+    let iLines = filter (not . null) $ lines input
+        games = map (readGame . pack) iLines
+    print $ sum $ map gId $ filter (gamePossibleWith (12, 13, 14)) games
+
