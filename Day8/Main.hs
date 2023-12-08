25dd1a4581dcfb35cb224a2378212689e7340ac2
Ashton Ohms <me@ashtonoh.ms>
2023-12-07 23:07:15 -0800

    Day 8, part 2

--- Day8/Main.hs
+++ Day8/Main.hs
@@ -1,6 +1,7 @@
 module Main (main) where
 
 import Data.Functor (($>))
+import Data.List (foldl')
 import Data.Map (Map)
 import qualified Data.Map as Map
 
@@ -15,11 +16,11 @@ dir = try (char 'L' $> L)
 
 entry :: Parsec String () (String, (String, String))
 entry = do
-    src <- count 3 letter
+    src <- count 3 (try letter <|> digit)
     _ <- string " = ("
-    dst1 <- count 3 letter
+    dst1 <- count 3 (try letter <|> digit)
     _ <- string ", "
-    dst2 <- count 3 letter
+    dst2 <- count 3 (try letter <|> digit)
     _ <- char ')'
     _ <- newline
     pure (src, (dst1, dst2))
@@ -31,14 +32,15 @@ inputP = do
     es <- many1 entry
     pure (dirs, Map.fromList es)
 
-travelToZZZ :: Network -> [Dir] -> String -> [String]
-travelToZZZ _ _ "ZZZ" = []
-travelToZZZ n (d:ds) node = node:travelToZZZ n ds (
+travelTo :: (String -> Bool) -> Network -> [Dir] -> String -> [String]
+travelTo f n (d:ds) node 
+  | f node = []
+  | otherwise = node:travelTo f n ds (
         case d of
           L -> lNode
           R -> rNode)
     where (lNode, rNode) = n Map.! node
-travelToZZZ _ [] _ = error "no more directions!"
+travelTo _ _ [] _ = error "no more directions!"
 
 main :: IO ()
 main = do
@@ -49,4 +51,7 @@ main = do
               Left e -> error $ show e
               Right v -> v
 
-    print $ length $ travelToZZZ network (cycle dirs) "AAA"
+        startNodes = filter (\node -> last node == 'A') $ Map.keys network
+
+    let pathLengths = map (length . travelTo (\s -> last s=='Z') network (cycle dirs)) startNodes
+    print $ foldl' lcm 1 pathLengths
