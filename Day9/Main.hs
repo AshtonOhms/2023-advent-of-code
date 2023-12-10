6341b5f5047ff018a1ba1c9e250025b65ad63937
Ashton Ohms <me@ashtonoh.ms>
2023-12-09 16:37:21 -0800

    Probably too terse

--- Day9/Main.hs
+++ Day9/Main.hs
@@ -2,33 +2,26 @@ module Main (main) where
 
 import Text.Parsec
 import Data.Functor (($>))
-import Control.Applicative (liftA2)
 
 inputP :: Parsec String () [[Int]]
 inputP = manyTill ((num `sepBy1` char ' ')<*newline) eof
-    where num = liftA2 (*)
-                   (option 1 (char '-'$>(-1)))
-                   (read <$> many1 digit)
+    where num = (*) <$> option 1 (char '-'$>(-1))
+                    <*> (read <$> many1 digit)
 
 diffs :: [Int] -> [Int]
 diffs (a:b:ns) = b-a:diffs (b:ns)
-diffs [_] = []
-diffs [] = []
+diffs _ = []
 
-extrap :: [Int] -> Int
-extrap ns 
+extrap :: ([Int] -> Int -> Int) -> [Int] -> Int
+extrap f ns
   | all (==0) ns = 0
-  | otherwise = last ns + extrap (diffs ns)
-
-extrap' :: [Int] -> Int
-extrap' ns
-  | all (==0) ns = 0
-  | otherwise = head ns - extrap' (diffs ns)
+  | otherwise = f ns $ extrap f (diffs ns)
 
 main :: IO ()
 main = do
     input <- readFile "input.txt"
     let sequences = either (error . show) id $
                         parse inputP "" input
-        solutions = sum . flip map sequences <$> [extrap, extrap'] 
+        solutions = sum . flip map sequences . extrap 
+                <$> [ (+) . last, (-) . head ]
     mapM_ print solutions
