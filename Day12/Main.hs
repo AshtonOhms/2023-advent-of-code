47b12f0807db5d0685a99ab62906fd3e7d807927
Ashton Ohms <me@ashtonoh.ms>
2023-12-16 18:28:22 -0800

    Day 12, part 2

--- Day12/Main.hs
+++ Day12/Main.hs
@@ -1,49 +1,49 @@
 module Main where
 
-import Data.Foldable
+import Control.Monad.Memo
+import Data.List (intercalate)
 import Text.Parsec
 
-import Debug.Trace
-
---traceShow b a = a
-
 inputP :: Parsec String () [([Char], [Int])]
-inputP = many1 $ (,) 
+inputP = many1 $ (,)
     <$> many1 (oneOf ".#?")<*char ' '
     <*> ((read <$> many1 digit) `sepBy` char ',')<* newline
 
 
-possib :: [Char] -> [Int] -> Int
+possib :: [Char] -> [Int] -> Memo (Int, [Char], [Int]) Int Int
 possib = possib' 0
-    where possib' :: Int -> [Char] -> [Int] -> Int
+    where possib' :: Int -> [Char] -> [Int] -> Memo (Int, [Char], [Int]) Int Int
           possib' run (c:cs) (g:gs)
-                | run > g = 0
-                | c == '.' = traceShow (run, c:cs, g:gs) $ 
-                                if run == 0 then possib' 0 cs (g:gs)
-                                else if run == g then possib' 0 cs gs
-                                else traceShow "wrong run size" 0
+                | run > g = return 0
+                | c == '.' = if run == 0 then for3 memo possib' 0 cs (g:gs)
+                             else if run == g then for3 memo possib' 0 cs gs
+                             else return 0
 
-                | c == '#' = traceShow (run, c:cs, g:gs) $ 
-                                possib' (run+1) cs (g:gs)
+                | c == '#' = possib' (run+1) cs (g:gs)
 
-                | c == '?' = traceShow (run, c:cs, g:gs) 
-                                (possib' run ('#':cs) (g:gs)
-                               + possib' run ('.':cs) (g:gs))
+                | c == '?' = do
+                              a <- for3 memo possib' run ('#':cs) (g:gs)
+                              b <- for3 memo possib' run ('.':cs) (g:gs)
+                              return $ a+b
 
                 | otherwise = error "unknown char"
 
-          possib' run [] [g] = traceShow (run, [] :: [Int], [g]) $
-                                if run==g then 1 else 0
-          possib' _ [] [] = traceShow "valid!" 1
-          possib' _ cs [] = traceShow "nums empty" $
-              if all (\c -> c =='.' || c == '?') cs then 1 else 0 
-          possib' _ [] _ = traceShow "cs empty" 0
+          possib' run [] [g] = return $ if run==g then 1 else 0
+          possib' _ [] [] = return 1
+          possib' _ cs [] = return $ if all (\c -> c =='.' || c == '?') cs then 1 else 0 
+          possib' _ [] _ = return 0
+
+unfold :: ([Char], [Int]) -> ([Char], [Int])
+unfold (cs, gs) = ( intercalate "?" (replicate 5 cs)
+                  , concat (replicate 5 gs))
 
 main :: IO ()
 main = do
     input <- readFile "input.txt"
     let rows = either (error . show) id (parse inputP "" input)
-    --print $ uncurry possib $ last rows -- !! 3
-    --for_ rows $ \r -> do
-    --    print $ uncurry possib r
-    print $ sum $ map (uncurry possib) rows
+
+    -- Part 1
+    --print $ sum $ map (startEvalMemo . uncurry possib) rows
+    
+    -- Part 2
+    print $ sum $ map (startEvalMemo . uncurry possib . unfold) rows
