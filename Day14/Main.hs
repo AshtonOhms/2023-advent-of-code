c360d408725134c9a79973b4bfb7842e9ef256b2
Ashton Ohms <me@ashtonoh.ms>
2023-12-17 18:11:10 -0800

    Day 14, part 2

--- Day14/Main.hs
+++ Day14/Main.hs
@@ -1,28 +1,53 @@
 module Main (main) where
 
+import Data.Foldable
 import Data.List
 import Data.List.Split
+import qualified Data.Set.Ordered as OSet
 
-compareRock :: Char -> Char -> Ordering
-compareRock 'O' '.' = LT
-compareRock '.' 'O' = GT
-compareRock a b = compare a b
+compareRock :: Bool -> Char -> Char -> Ordering
+compareRock False 'O' '.' = LT
+compareRock False '.' 'O' = GT
+compareRock True 'O' '.' = LT
+compareRock True '.' 'O' = GT
+compareRock _ a b = compare a b
 
-slideCol :: [Char] -> [Char]
-slideCol cs = intercalate "#" 
-            $ map (sortBy compareRock) 
-            $ splitOn "#" cs
+slideCol :: Bool -> [Char] -> [Char]
+slideCol r cs = intercalate "#" 
+              $ map (sortBy $ compareRock r) 
+              $ splitOn "#" cs
 
 scoreCol :: [Char] -> Int
 scoreCol = sum . zipWith (\s c -> if c=='O' then s else 0) [1..] . reverse
 
+data Dir = N | W | S | E deriving (Show, Eq, Ord)
+
+slide :: Dir -> [[Char]] -> [[Char]]
+slide W = map (slideCol False)
+slide E = map (reverse . slideCol False . reverse)
+slide N = transpose . map (slideCol False) . transpose
+slide S = transpose . map (reverse . slideCol False . reverse) . transpose
+
+load :: [[Char]] -> Int
+load = sum . map scoreCol . transpose
+
+-- finds the start and length of cycle in the sequence
+-- returns (# states before cycle, # states in cycle, [states])
+findCycle :: [[Char]] -> (Int, Int, [[[Char]]])
+findCycle = findCycle' OSet.empty
+    where findCycle' seen pattern
+            | Just i <- pattern `OSet.findIndex` seen = 
+                (i, length seen - i, drop i $ toList seen)
+            | otherwise = findCycle'
+                          (seen OSet.|> pattern)
+                          (foldl' (flip slide) pattern [N,W,S,E])
+
+patternAtCycle :: [[Char]] -> Int -> [[Char]]
+patternAtCycle pattern cycles = states !! cycles'
+    where (preCycle, cycleLength, states) = findCycle pattern
+          cycles' = (cycles - preCycle) `mod` cycleLength
+
 main :: IO ()
 main = do
     input <- lines <$> readFile "input.txt"
-    let cols = transpose input
-        slidCols = map slideCol cols
-        slid = transpose slidCols
-
-    mapM_ putStrLn slid
-
-    print $ sum $ map (scoreCol . slideCol) cols
+    print $ load $ patternAtCycle input 1000000000
